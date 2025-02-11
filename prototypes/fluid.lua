8ffa02231de348336cba8dd6431faa54eda0e016
Suuma <30609754+suumani@users.noreply.github.com>
2025-02-12 00:17:24 +0900

    (ver.0.0.3) init

---
+++ prototypes/fluid.lua
@@ -0,0 +1,51 @@
+
+data:extend({
+	-- ----------------------------
+	-- 水素（Hydrogen）
+	-- ----------------------------
+	{
+		type = "fluid",
+		name = "hydrogen",
+		localised_name = {"item-name.hydrogen"},
+		localised_description = {"item-description.hydrogen"},
+		subgroup = "fluid",
+		default_temperature = 15,
+		base_color = {r=0.1, g=0.6, b=1.0}, 
+		flow_color = {r=0.3, g=0.8, b=1.0}, 
+		icon = "__ValentineChocolateFactory__/graphics/icons/hydrogen.png",
+		order = "a[fluid]-z[hydrogen]-z[hydrogen]"
+	}
+	,
+	-- ----------------------------
+	-- 一酸化炭素（Carbon Monoxide）
+	-- ----------------------------
+	{
+		type = "fluid",
+		name = "carbon_monoxide",
+		localised_name = {"item-name.carbon_monoxide"},
+		localised_description = {"item-description.carbon_monoxide"},
+		subgroup = "fluid",
+		default_temperature = 15,
+		base_color = {r=0.3, g=0.3, b=0.3},
+		flow_color = {r=0.8, g=0.8, b=0.8},
+		icon = "__ValentineChocolateFactory__/graphics/icons/carbon_monoxide.png",
+		order = "a[fluid]-z[carbon_monoxide]-z[carbon_monoxide]"
+	}
+	,
+	-- ----------------------------
+	-- バニラエッセンス（Vanilla Essence）
+	-- ----------------------------
+	{
+		type = "fluid",
+		name = "vanilla_essence",
+		localised_name = {"item-name.vanilla_essence"},
+		localised_description = {"item-description.vanilla_essence"},
+		subgroup = "fluid",
+		default_temperature = 15,
+		base_color = {r=0.3, g=0.3, b=0.3},
+		flow_color = {r=0.8, g=0.8, b=0.8},
+		icon = "__ValentineChocolateFactory__/graphics/icons/vanilla_essence.png",
+		order = "a[fluid]-z[vanilla_essence]-z[vanilla_essence]"
+	}
+	,
+})
